995e4936279e6b927a3bdc129a30de019e785c4e
Abraham Ragab <Abrahammoustafa@hotmail.co.uk>
2018-12-22 16:09:56 +0200

    fetch handle data using fetch();

--- lesson-3-async-w-fetch/app.js
+++ lesson-3-async-w-fetch/app.js
@@ -8,5 +8,35 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+    fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
+        headers: {
+            Authorization: 'Client-ID 5fd98b156b2293d680d4560d3024f2b55c19353c9aa8cadf29e3244b1054bcca'
+        }
+    }).then(response => response.json())
+    .then(addImage)
+    .catch(e => requestError(e, 'image'));
+
+
+    function addImage(data) {
+        let htmlContent = '';
+        const firstImage = data.results[0];
+    
+        if (firstImage) {
+            htmlContent = `<figure>
+                <img src="${firstImage.urls.small}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`;
+        } else {
+            htmlContent = 'Unfortunately, no image was returned for your search.'
+        }
+    
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+    
+    function requestError(e, part) {
+        console.log(e);
+        responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning">Oh no! There was an error making a request for the ${part}.</p>`);
+    }
     });
 })();
