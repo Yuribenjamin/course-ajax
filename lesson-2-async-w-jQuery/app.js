5be9c1dadea695425e36d3682f51fc6efff9ffdc
Abraham Ragab <Abrahammoustafa@hotmail.co.uk>
2018-12-22 15:18:20 +0200

    Fetch and handle data with jQuery

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -10,5 +10,50 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+    $.ajax({
+        url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+        headers: {
+            Authorization: 'Client-ID 5fd98b156b2293d680d4560d3024f2b55c19353c9aa8cadf29e3244b1054bcca'
+        }
+    }).done(addImage)
+    .fail(function (err) {
+        requestError(err, 'image');
+    });
+    /**
+     * This function handle same operation the below function did
+     * but if there's no image founded there's nothing will display
+     * compare with the below function it return message if no images
+     * Avalible
+     * 
+     function addImage(images) {
+        const firstImage = images.results[0];
+
+        responseContainer.insertAdjacentHTML('afterbegin', `<figure>
+                <img src="${firstImage.urls.small}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`
+        );
+        }
+     */
+
+     
+    function addImage(data) {
+        let htmlContent = "";
+    
+        if(data && data.results && data.results.length > 1){
+            const firstImage = data.results[0];
+            htmlContent = `<figure>
+                <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            <figure>`;
+        }else {
+            htmlContent = '<div class="error-no-image">No images availble</div>';
+        }
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent)
+    }
+    function requestError(e, part) {
+        console.log(e, part);
+    }
     });
 })();
